2d86fb89ec67214792691175c974590e79cf2c43
vionarahmawati <112797745+vionarahmawati@users.noreply.github.com>
2022-09-11 12:03:55 +0700

    Add files via upload

---
+++ p03jquery/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.nama_mhs').click(function(){
+        let isi = $(this).text();
+
+        let isi2 = prompt('Ubah dengan:', isi);
+        if(!isi2) return;
+
+        $(this).text(isi2);
+    });
+
+    $('.hapus').click(function(){
+        let yakin = confirm('Apakah anda yakin ingin menghapus?');
+        if(!yakin) return;
+        
+        let id = $(this).prop('id');
+        let rid = id .split('__');
+        let baris_ke = rid[1];
+
+        $('#row__'+baris_ke).slideUp();
+    });
+
+    $('.alamat').click(function (event) {
+        alert("Anda mengklik kolom yang berisi kata " + $(this).html() + "!");
+    });
+
+    $("th").click(function (event) {
+        alert("Anda mengklik kolom judul yang berisi kata " + $(this).html() + "!");
+    });
+});
